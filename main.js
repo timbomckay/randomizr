b07c0d5bb26ea2d46d49d75a68c977f1f4e0265e
Tim McKay <timbomckay@gmail.com>
2016-12-25 02:34:21 -0500

    Adding localStorage, list management & fullscreen

--- main.js
+++ main.js
@@ -1,68 +1,35 @@
+// Find the right method, call on correct element
+function launchFullscreen(element) {
+		if (element.requestFullscreen) {
+				element.requestFullscreen();
+		} else if (element.mozRequestFullScreen) {
+				element.mozRequestFullScreen();
+		} else if (element.webkitRequestFullscreen) {
+				element.webkitRequestFullscreen();
+		} else if (element.msRequestFullscreen) {
+				element.msRequestFullscreen();
+		}
+}
+
+function exitFullscreen() {
+		if (document.exitFullscreen) {
+				document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+		}
+}
+
+function dumpFullscreen() {
+		console.log("document.fullscreenElement is: ", document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
+		console.log("document.fullscreenEnabled is: ", document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled || document.msFullscreenEnabled);
+}
+
+
 /* jQuery(document).ready(function($) {
-	// Find the right method, call on correct element
-	function launchFullscreen(element) {
-	    if (element.requestFullscreen) {
-	        element.requestFullscreen();
-	    } else if (element.mozRequestFullScreen) {
-	        element.mozRequestFullScreen();
-	    } else if (element.webkitRequestFullscreen) {
-	        element.webkitRequestFullscreen();
-	    } else if (element.msRequestFullscreen) {
-	        element.msRequestFullscreen();
-	    }
-	}
-
-	function exitFullscreen() {
-	    if (document.exitFullscreen) {
-	        document.exitFullscreen();
-	    } else if (document.mozCancelFullScreen) {
-	        document.mozCancelFullScreen();
-	    } else if (document.webkitExitFullscreen) {
-	        document.webkitExitFullscreen();
-	    }
-	}
-
-	function dumpFullscreen() {
-	    console.log("document.fullscreenElement is: ", document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
-	    console.log("document.fullscreenEnabled is: ", document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled || document.msFullscreenEnabled);
-	}
-
-	function startTimer() {
-	    var regex = /\d{2}/g,
-	        matches = getTimer.match(regex),
-	        minutes = matches[0],
-	        seconds = matches[1],
-	        setTimer = new Date();
-
-	    setTimer.setMinutes(minutes);
-	    setTimer.setSeconds(seconds);
-
-	    timer = setInterval(function () {
-	        var totalMs = setTimer.getTime(),
-	            minutes, seconds;
-
-	        setTimer.setTime(totalMs - 1000);
-
-	        minutes = setTimer.getMinutes();
-	        seconds = setTimer.getSeconds();
-
-	        if (minutes === 0 && seconds === 0) {
-	            clearInterval(timer);
-	            if (lastItem === true) {
-	                $('#selection').text('All Done!');
-	            } else {
-	                newSelection();
-	            }
-	        }
-
-	        $("#timer").text(
-	        (minutes >= 10 ? minutes : "0" + minutes) + ":" + (seconds >= 10 ? seconds : "0" + seconds));
-
-	    }, 1000);
-	}
 
 	function newSelection() {
-	    //clearInterval(timer);
 	    var selection = items.sort(function () {
 	        return 0.5 - Math.random();
 	    }).pop();
@@ -79,17 +46,13 @@
 	        $('#selection').append('<span>Last One</span>');
 	    }
 
-	    //$("#timer").text(getTimer);
-	    // startTimer();
 	    if (items.length === 0) {
 	        lastItem = true;
 	    }
 	}
 
-	var getTimer = $("#timer").text(),
-	    items = new Array(),
-	    lastItem = false,
-	    timer;
+	var items = new Array(),
+	    lastItem = false;
 
 	// Add items to list
 	$('#list li').each(function () {
@@ -116,7 +79,6 @@
 	});
 
 	$('#next').click(function () {
-	    //clearInterval(timer);
 	    if (lastItem === true) {
 	        $('#selection').text('All Done!');
 	        $('#controls').remove();
@@ -128,44 +90,35 @@
 });
 */
 
+
+// localStorage persistence
+var STORAGE_KEY = 'randomizr'
+var listStorage = {
+  fetch: function () {
+    var people = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    people.forEach(function (person, index) {
+      person.id = index
+    })
+    listStorage.uid = people.length
+    return people
+  },
+  save: function (people) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(people))
+  }
+}
+
 var app = new Vue({
 	el: '#app',
   // app initial state
   data: {
-    // todos: todoStorage.fetch(),
-    // newTodo: '',
-    // editedTodo: null,
-    // visibility: 'all',
+		message: 'Randomizr!',
+    people: listStorage.fetch(),
+		presentation : false,
 
-		message: 'Hello Randomizr!',
-
-		people: [
-      {
-      	name: 'Kevin',
-      	count: 2
-      },
-      {
-      	name: 'Janet',
-      	count: 3
-      },
-      {
-      	name: 'John',
-      	count: 1
-      },
-      {
-      	name: 'Sarah',
-      	count: 4
-      },
-      {
-      	name: 'Tim',
-      	count: 2
-      },
-      {
-      	name: 'Karissa',
-      	count: 3
-      }
-
-		]
+		newPerson: {
+      name: '',
+      count: ''
+    },
 
 		// shared: [
 		// 	{
@@ -178,5 +131,46 @@ var app = new Vue({
 		// 	}
 		// ]
 
+  },
+
+  // watch people change for listStorage persistence
+  watch: {
+    people: {
+      handler: function (people) {
+        listStorage.save(people)
+      },
+      deep: true
+    }
+  },
+
+  // methods
+  methods: {
+    addPerson: function () {
+      if ( !this.newPerson.name || !this.newPerson.name.trim() || !this.newPerson.count ) {
+        return
+      }
+
+      app.people.push({
+        // id: listStorage.uid++,
+				id: app.people.uid++,
+        name: this.newPerson.name,
+        count: this.newPerson.count
+      })
+
+      this.newPerson.name = ''
+      this.newPerson.count = ''
+
+    },
+    removePerson: function (person) {
+      this.people.splice(this.people.indexOf(person), 1)
+    },
+		startRandomizr: function () {
+			launchFullscreen(document.documentElement)
+			app.presentation = true
+			window.addEventListener('keyup', function(e){
+				if(e.key == 'Escape') app.presentation = false;
+			})
+		}
   }
+
 });
